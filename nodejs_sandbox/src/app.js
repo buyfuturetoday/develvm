c44a65526b6bca3deda1fd11dc6a6875a9b41f2d
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-11-02 08:25:30 +0100

    Fix

--- nodejs_sandbox/src/app.js
+++ nodejs_sandbox/src/app.js
@@ -63,7 +63,9 @@
         var options = {}; // all options listed in the REST documentation for Docker are supported.
 
         docker.containers.list(options /* optional*/, function(err, res) {
-            if (err) throw err;
+            if (err) { 
+                throw err;
+            }
             console.log("data returned from Docker as JS object: ", res);
         });
 
