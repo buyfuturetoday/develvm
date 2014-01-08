5e9fddd45f2d25793e1075b1a1b5cd37c82de1d5
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2014-01-08 21:22:26 +0100

    Added

---
+++ couchdb-apps/curl-tests/config-template.js
@@ -0,0 +1,9 @@
+var COUCHDB_HOST        = "app1.jacc.local",
+    COUCHDB_PORT        = 5984,
+    COUCHDB_ADMIN       = "admin",
+    COUCHDB_ADMIN_PASS  = "mysecretpassword",
+    COUCHDB_USER        = "username",
+    COUCHDB_USER_PASS   = "xxx";
+
+var COUCHDB1            = "http://"+COUCHDB_ADMIN+":"+COUCHDB_ADMIN_PASS+"@"+COUCHDB_IP_AND_PORT,
+    COUCHDB2            = "http://"+COUCHDB_USER+":"+COUCHDB_USER_PASS+"@"+COUCHDB_IP_AND_PORT;
