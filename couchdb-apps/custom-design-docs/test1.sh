e7a294a0927a06296d591be592ea031a99ebf364
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2014-01-08 19:35:47 +0100

    Fix

--- couchdb-apps/custom-design-docs/test1.sh
+++ couchdb-apps/custom-design-docs/test1.sh
@@ -2,10 +2,27 @@
 
 source setenv
 
+# Delete database, in case it already exists
+CMD="curl -X DELETE ${COUCHDB1}/test/"
+echo $CMD
+time $CMD
+
+# Create database
 CMD="curl -X PUT ${COUCHDB1}/test/"
 echo $CMD
-$CMD
+time $CMD
 
+# Show database info
 CMD="curl -X GET ${COUCHDB1}/test/"
 echo $CMD
-$CMD
+time $CMD
+
+# Store empty document
+CMD="curl -X POST -H Content-Type:application/json -d {} ${COUCHDB1}/test/"
+echo $CMD
+time $CMD
+
+# List all documents
+CMD="curl -X GET ${COUCHDB1}/test/_all_docs"
+echo $CMD
+time $CMD
