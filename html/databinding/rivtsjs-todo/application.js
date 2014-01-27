bcead035ae4802ae5ae4226c974970e0c8660c46
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2014-01-27 19:25:02 +0100

    added

---
+++ html/databinding/rivtsjs-todo/application.js
@@ -0,0 +1,61 @@
+var Todos = Stapes.subclass({
+  all: function() {
+    return this.getAllAsArray()
+  },
+
+  active: function() {
+    return _.select(this.all(), function(todo) {
+      return !todo.get('done')
+    })
+  }
+})
+
+var Todo = Stapes.subclass({})
+
+var ViewModel = Stapes.subclass({
+  constructor: function(todos) {
+    this.todos = todos
+  },
+
+  init: function() {
+    _.bindAll(this, 'setTodos', 'showAll', 'showActive', 'setRemaining', 'add')
+    
+    this.todos.on('change', this.setTodos)
+    this.todos.on('change', this.setRemaining)
+    this.showAll()
+    this.setRemaining()
+  },
+
+  setTodos: function() {
+    this.set('todos', this.todos[this.get('filter')]())
+  },
+
+  showAll: function() {
+    this.set('filter', 'all')
+    this.setTodos()
+  },
+
+  showActive: function() {
+    this.set('filter', 'active')
+    this.setTodos()
+  },
+
+  setRemaining: function() {
+    this.set('remaining', this.todos.active().length)
+  },
+
+  add: function(ev) {
+    if(ev.keyCode === 13) {
+      todo = new Todo()
+      todo.set('summary', ev.currentTarget.value)
+      ev.currentTarget.value = ''
+      this.todos.push(todo)
+    }
+  }
+})
+
+$(document).ready(function() {
+  app = new ViewModel(new Todos())
+  app.init()
+  rivets.bind($('#todo-list'), {app: app})
+})
