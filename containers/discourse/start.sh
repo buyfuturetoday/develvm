3a83d6f7359c43c34bc0eb2eb72647df003b280b
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2014-02-01 13:33:33 +0100

    Fix

--- containers/discourse/start.sh
+++ containers/discourse/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-postgres -c /usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf
+# postgres -c /usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf
+
+# These don't work with supervisor
 service syslog-ng start ; service postfix start
 
 supervisord -n
