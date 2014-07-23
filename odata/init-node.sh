fbc827c3a12b700d09370f2be9c39dbcb987b4ee
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2014-07-23 22:46:07 +0200

    update

--- odata/init-node.sh
+++ odata/init-node.sh
@@ -6,4 +6,4 @@ if [ ! -f /usr/bin/node ]; then
     source $HOME/.nvm/nvm.sh; nvm use v0.11.2; n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr
 fi
 
-source $HOME/.nvm/nvm.sh; nvm use v0.11.2; cd /; npm install -g
+source $HOME/.nvm/nvm.sh; nvm use v0.11.2; cd /src; npm install -g
